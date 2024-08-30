2c5c45875625ebc6d6991365b01932265ce187e1
Calvin Huang <jibunhidoi999@gmail.com>
2024-08-30 01:25:48 +0000

    Added some helpful scripts

---
+++ scripts/git_author_rewrite.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git filter-branch --env-filter '
+OLD_EMAIL="calvin.huang@linkdea.com"
+CORRECT_NAME="Calvin Huang"
+CORRECT_EMAIL="jibunhidoi999@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
